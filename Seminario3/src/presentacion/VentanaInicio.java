a9fed6278d74f9a174c82e1730d46dfb30b9f3f1
zykler <javier.gceca@gmail.com>
2013-11-05 13:19:21 +0100

    Añadidos los botones Limpiar y Guardar.

--- Seminario3/src/presentacion/VentanaInicio.java
+++ Seminario3/src/presentacion/VentanaInicio.java
@@ -3,10 +3,17 @@ package presentacion;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import javax.swing.JButton;
 
 public class VentanaInicio {
 
 	private JFrame frame;
+	private final JPanel panel = new JPanel();
+	private final JButton btnLimpiar = new JButton("Limpiar");
+	private final JButton btnGuardar = new JButton("Guardar");
 
 	/**
 	 * Launch the application.
@@ -38,6 +45,16 @@ public class VentanaInicio {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		{
+			FlowLayout flowLayout = (FlowLayout) panel.getLayout();
+			frame.getContentPane().add(panel, BorderLayout.SOUTH);
+		}
+		{
+			panel.add(btnLimpiar);
+		}
+		{
+			panel.add(btnGuardar);
+		}
 	}
 
 }
